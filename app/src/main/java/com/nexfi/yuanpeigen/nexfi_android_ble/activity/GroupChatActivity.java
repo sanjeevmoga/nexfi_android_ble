dbae8858ec4b53178d5e4a356428b3e2407c8d90
yuanpeigen <420478741@qq.com>
2016-04-13 14:32:40 +0800

    新增图标

---
+++ app/src/main/java/com/nexfi/yuanpeigen/nexfi_android_ble/activity/GroupChatActivity.java
@@ -0,0 +1,19 @@
+package com.nexfi.yuanpeigen.nexfi_android_ble.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nexfi.yuanpeigen.nexfi_android_ble.R;
+
+/**
+ * Created by Mark on 2016/4/13.
+ */
+public class GroupChatActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groupchat);
+    }
+}
